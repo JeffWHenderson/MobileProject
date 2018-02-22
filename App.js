2743aba6de40336eb5bd939bdbe1601afd6ad66a
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-21 19:08:34 -0500

    added some folder structure and the Bananas Component example to test on phone.

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Bananas from './components/Bananas'
 
 export default class App extends React.Component {
   render() {
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Bananas />
       </View>
     );
   }
@@ -16,7 +17,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fca',
     alignItems: 'center',
     justifyContent: 'center',
   },
